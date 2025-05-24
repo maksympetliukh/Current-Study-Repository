684d8c1d4d2e297d46508a1571f9566180e1c129
maksympetliukh <maksympetliukh@icloud.com>
2025-05-24 12:30:19 +0200

    1

--- study.cpp
+++ study.cpp
@@ -4,7 +4,17 @@ using std::cout;
 using std::cin;
 using std::hex;
 using std:: endl;
+using std::boolalpha;
 
 int main(){
-    
+    int a {10};
+    int b {4};
+
+    if((a + b) < 15){
+        cout << "Statement is true" << endl;
+    }else{
+        cout << "Statement is false" << endl;
+    }
+
+    return 0;
 }
